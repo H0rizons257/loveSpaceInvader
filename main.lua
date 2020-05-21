fcf9dcacd8c4776fff030dfd3a141431157e6069
simon <faubert.simon15@hotmail.com>
2020-05-21 11:53:06 -0400

    at1

--- main.lua
+++ main.lua
@@ -1,3 +1,4 @@
+require("player")
 
 SCALE = 0.5
 BGM_VOLUME_HIGH = 0.2
@@ -8,13 +9,22 @@ RELOAD_TIME = 0.7
 
 
 function love.load()
+	love.window.setTitle("Space Invader")
+	love.window.setMode(SCREEN_WIDTH, SCREEN_HEIGHT)
+	
+	playerShip = createPlayer()
 end
 	
 
-function love.update(dt)	
+function love.update(dt)
+	if love.keyboard.isDown("left") then
+		playerShip.move(-dt)
+	elseif love.keyboard.isDown("right") then
+		playerShip.move(dt)
+	end
 end
 
 function love.draw()
-	love.graphics.print("Hello World", 0, 0)
+	love.graphics.draw(playerShip.sprite, playerShip.x, playerShip.y, 0, SCALE, SCALE)
 end
 
