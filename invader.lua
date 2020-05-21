59e91860215bde5867ccbc4e83fa68bbc37b2d70
simon <faubert.simon15@hotmail.com>
2020-05-21 13:07:25 -0400

    at2

--- invader.lua
+++ invader.lua
@@ -5,5 +5,31 @@ function createInvader(row, column)
 	invader.sprite = love.graphics.newImage("sprites/enemy"..row.."_"..column..".png")
 	invader.speed = 130
 
+	invader.x = invader.width/2 + invader.width*column
+	invader.y = row*invader.height
+	direction = 1
+	
 	return invader
+end
+
+function moveInvader(invader, dt)
+	invader.x = invader.x + (invader.speed * dt * direction)
+	if changeDirection then
+		invader.y = invader.y + invader.height
+		if invader.y + invader.height > SCREEN_HEIGHT - playerShip.height then
+			gameOver = true
+		end
+	end
+end
+
+function detectSide(invader)	
+	if invader.x <= 0 then
+		direction = 1
+		return true
+	end
+	if invader.x >= SCREEN_WIDTH - invader.width  then
+		direction = -1
+		return true
+	end
+	return false
 end
--- main.lua
+++ main.lua
@@ -1,4 +1,5 @@
 require("player")
+require("invader")
 
 SCALE = 0.5
 BGM_VOLUME_HIGH = 0.2
@@ -13,18 +14,56 @@ function love.load()
 	love.window.setMode(SCREEN_WIDTH, SCREEN_HEIGHT)
 	
 	playerShip = createPlayer()
+	
+	enemies = {}
+	for row = 1, 4 do
+		enemies[row] = {}
+		for column = 1, 5 do
+			enemies[row][column] = createInvader(row, column)
+		end
+	end
 end
 	
+gameOver = false
 
 function love.update(dt)
-	if love.keyboard.isDown("left") then
-		playerShip.move(-dt)
-	elseif love.keyboard.isDown("right") then
-		playerShip.move(dt)
+	if gameOver == false then
+	
+		if love.keyboard.isDown("left") then
+			playerShip.move(-dt)
+		elseif love.keyboard.isDown("right") then
+			playerShip.move(dt)
+		end
+		
+		changeDirection = false
+		for row=1,#enemies do
+			for column=1,#enemies[row] do
+				if(changeDirection == false) then
+					changeDirection = detectSide(enemies[row][column])
+				end
+			end
+		end
+		
+		for row=1,#enemies do
+			for column=1,#enemies[row] do
+				moveInvader(enemies[row][column], dt)
+			end
+		end
 	end
 end
 
 function love.draw()
 	love.graphics.draw(playerShip.sprite, playerShip.x, playerShip.y, 0, SCALE, SCALE)
+	
+	if gameOver then 
+		love.graphics.print("Game Over", 0, 0)
+	end
+	
+	for row=1,#enemies do
+        for column=1,#enemies[row] do
+			local invader = enemies[row][column]
+			love.graphics.draw(invader.sprite, invader.x, invader.y, 0, SCALE, SCALE)
+		end
+	end
 end
 
