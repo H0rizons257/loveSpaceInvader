0bd30aec9c9fc316ae919fb28c27ce88998dc587
simon <faubert.simon15@hotmail.com>
2020-05-21 11:30:24 -0400

    at0

--- invader.lua
+++ invader.lua
@@ -1,54 +1,9 @@
 function createInvader(row, column)
-invader = {}
-invader.width=52
-invader.height=42
-invader.sprite = love.graphics.newImage("sprites/enemy"..row.."_"..column..".png")
-invader.speed = 130
+	invader = {}
+	invader.width=52
+	invader.height=42
+	invader.sprite = love.graphics.newImage("sprites/enemy"..row.."_"..column..".png")
+	invader.speed = 130
 
-invader.x = invader.width/2 + invader.width*column
-invader.y = row*invader.height
-invader.alive = true
-direction = 1
-
-return invader
-end
-
-function moveInvader(invader, dt)
-	invader.x = invader.x + (invader.speed * dt * direction)
-	if changeDirection then
-		invader.y = invader.y + invader.height
-		if invader.y + invader.height > SCREEN_HEIGHT - playerShip.height then
-			gameOver = true
-		end
-	end
-end
-
-function detectSide(invader)
-	if invader.alive then
-		if invader.x <= 0 then
-			direction = 1
-			return true
-		end
-		if invader.x >= SCREEN_WIDTH - invader.width  then
-			direction = -1
-			return true
-		end
-	end
-	return false
-end
-
-
-function detectLaserCollision(invader)
-	for currentLaser=1,#lasers do
-		local laser = lasers[currentLaser]
-		if (laser.x + laser.width > invader.x and laser.x < invader.x + invader.width) and 
-		(laser.y + laser.height > invader.y and laser.y < invader.y + invader.height) and 
-		invader.alive == true
-		then
-			invader.alive = false
-			table.remove(lasers, currentLaser)
-			score = score + 200
-			break
-		end		
-	end
+	return invader
 end
